e915f805e946a80396f142c7aaad3a4adab16847
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-09-14 07:01:23 -0300

    feat(task): Adiciona metodo para listar todas as tarefas

--- src/main/java/com/phthedev/ToDoList/Task/TaskRepository.java
+++ src/main/java/com/phthedev/ToDoList/Task/TaskRepository.java
@@ -2,7 +2,9 @@ package com.phthedev.ToDoList.Task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
 }
